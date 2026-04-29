5d23c7be728168070247fa0d634b55f9cc35d3e9
George <georgeza@udel.edu>
2026-04-29 16:39:36 -0400

    Secret changes

--- src/game/scenes/secret.ts
+++ src/game/scenes/secret.ts
@@ -9,6 +9,15 @@ export class Secret extends Scene {
     private shoes: Phaser.Physics.Arcade.Image;
     private wand: Phaser.Physics.Arcade.Image;
     private crown: Phaser.Physics.Arcade.Image;
+    private elevator: Phaser.Physics.Arcade.Image;
+
+    private hasShoes: boolean;
+    private hasWand: boolean;
+    private hasCrown: boolean;
+    private bartShoes: boolean;
+    private bartWand: boolean;
+    private bartCrown: boolean;
+    private elevatorOpen: boolean;
 
     create() {}
 
@@ -19,6 +28,14 @@ export class Secret extends Scene {
     handleShoes() {}
 
     handleCrown() {}
+
+    openElevator() {
+        if (this.hasShoes && this.hasWand && this.hasCrown) {
+            this.elevatorOpen = true;
+        } else {
+            this.elevatorOpen = false;
+        }
+    }
 
     update() {}
 
