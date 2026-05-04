1d2f5faf2a9a9323173563f9e832d2883b242695
George <georgeza@udel.edu>
2026-05-04 12:12:01 -0400

    Created the baseline for level5

--- src/game/scenes/level5.ts
+++ src/game/scenes/level5.ts
@@ -15,6 +15,7 @@ export class Level5 extends Scene {
     private motionsensor: Phaser.Physics.Arcade.Image;
     private door: Phaser.Physics.Arcade.Image;
 
+    private hasHammer: boolean;
     private thronebroken: boolean;
     private msActivated: boolean;
     private doorOpened: boolean;
@@ -24,13 +25,69 @@ export class Level5 extends Scene {
         EventBus.emit("current-scene-ready", this);
     }
 
-    private breakThrone() {}
+    private acquireHammer() {
+        if (!this.physics.overlap(this.player, this.hammer)) {
+            this.hasHammer = false;
+        } else if (
+            this.physics.overlap(this.player, this.hammer) &&
+            !this.hasHammer
+        ) {
+            this.hasHammer = true;
+        } else {
+            this.hasHammer = false;
+        }
+    }
 
-    private activateMS() {}
+    private breakThrone() {
+        if (!this.hasHammer) {
+            this.thronebroken = false;
+        }
+        if (
+            this.hasHammer &&
+            this.physics.overlap(this.player, this.throne) &&
+            !this.thronebroken
+        ) {
+            this.thronebroken = true;
+        } else {
+            this.thronebroken = false;
+        }
+    }
 
-    private openDoor() {}
+    private activateMS() {
+        if (!this.thronebroken) {
+            this.msActivated = false;
+        }
+        if (
+            this.thronebroken &&
+            this.physics.overlap(this.player, this.motionsensor)
+        ) {
+            this.msActivated = true;
+        } else {
+            this.msActivated = false;
+        }
+    }
 
-    update() {}
+    private openDoor() {
+        if (!this.msActivated) {
+            this.doorOpened = false;
+        }
+        if (this.msActivated && this.physics.overlap(this.player, this.door)) {
+            this.doorOpened = true;
+        } else {
+            this.doorOpened = false;
+        }
+        if (this.doorOpened) {
+            this.fakeWin = true;
+        }
+    }
 
-    changeScene() {}
+    update() {
+        this.fpsText.update();
+    }
+
+    changeScene() {
+        if (this.fakeWin) {
+            this.scene.start("PlotTwist");
+        }
+    }
 }
