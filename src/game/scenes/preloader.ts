86b4cd2477e662ed412761cfd7e8aa2cb0299f3e
George <georgeza@udel.edu>
2026-05-14 13:37:24 -0400

    Created the assets, check to see if everything lines up

--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -54,28 +54,56 @@ export class Preloader extends Scene {
         ); /* Credit goes to https://opengameart.org/content/wall by author djonvincent */
         this.load.image(
             "prisoncells",
-            "assets/prisoncell.png",
-        ); /* Credit goes to https://opengameart.org/content/prison-tiles by author devnewton */
+            "assets/Cells.png",
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
-            "Crowbar.txt",
-            "assets/crowbar.png",
-        ); /* Credit goes to https://opengameart.org/content/crow-bar by author Lewis W. Veasey */
+            "Crowbar.exe",
+            "assets/Crowbar.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "crow",
+            "assets/crow.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "bar",
+            "assets/bar.png",
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "chain",
             "assets/chain.png",
-        ); /* Credit goes to https://opengameart.org/content/chain-whip by Nineball */
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "gloves",
             "assets/gloves.png",
         ); /* Credit goes to https://opengameart.org/content/pointers-part-5 by yd */
         this.load.image(
+            "dirt",
+            "assets/dirt.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "candle",
+            "assets/candle.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "flask",
+            "assets/flash.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "Hole",
+            "assets/Hole.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "Potions",
+            "assets/potions.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
             "guillotine",
             "assets/guillotine.png",
-        ); /* Credit goes to https://opengameart.org/content/guillotine by author Rudy Phelippeau */
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "lever",
             "assets/lever.png",
-        ); /* Credit goes to https://opengameart.org/content/bundle-events-assets by amaralzin9309 */
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "flasks",
             "assets/flasks.png",
@@ -87,16 +115,23 @@ export class Preloader extends Scene {
         this.load.image(
             "key",
             "assets/key.png",
-        ); /* Credit goes to https://opengameart.org/content/rusty-iron-key-povray-scene-file by Varkalandar */
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "door",
             "assets/door.png",
         ); /* Credit goes to https://opengameart.org/content/wood-door by wobba89 */
-        this.load.image("boxes", "assets/boxes.png");
-        this.load.image("buttons", "assets/buttons.png");
-        this.load.image("throne", "assets/throne.png");
-        this.load.image("motionsensor", "assets/motionsensor.png");
-        this.load.image("hammer", "assets/hammer.png");
+        this.load.image(
+            "boxes",
+            "assets/box.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "throne",
+            "assets/throne.png",
+        ); /* Credit goes to Leif Kaine */
+        this.load.image(
+            "motionsensor",
+            "assets/detector.png",
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "fountain",
             "assets/fountain.png",
@@ -108,11 +143,11 @@ export class Preloader extends Scene {
         this.load.image(
             "books",
             "assets/book.png",
-        ); /* Credit goes to https://opengameart.org/content/spell-book by AnthonyMyers */
+        ); /* Credit goes to Leif Kaine */
         this.load.image(
             "table",
             "assets/table.png",
-        ); /* Credit goes to https://opengameart.org/content/vendors-table-lpc-table-rework by AntumDeluge */
+        ); /* Credit goes to Leif Kaine */
         this.load.image("logo", "assets/logo.png");
         this.load.image("star", "assets/star.png");
         this.load.image("phaser-logo", "assets/phaser-logo.png");
