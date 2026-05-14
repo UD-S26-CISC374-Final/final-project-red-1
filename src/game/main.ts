5c2af092595c8b05445e4dbcaea356c4ab83608c
DopoTSLol <kdopo@udel.edu>
2026-05-14 18:05:58 -0400

    Deleted unused files

--- src/game/main.ts
+++ src/game/main.ts
@@ -4,10 +4,7 @@ import { Tutorial } from "./scenes/tutorial";
 import { Level1 as Level1Game } from "./scenes/level1";
 import { Level2 as Level2Game } from "./scenes/level2";
 import { Level3 as Level3Game } from "./scenes/level3";
-//import { Level4 as Level4Game } from "./scenes/level4";
 import { Level5 as Level5Game } from "./scenes/level5";
-import { PlotTwist } from "./scenes/plottwist";
-import { Secret } from "./scenes/secret";
 import { Win } from "./scenes/win";
 
 import { Terminal } from "./scenes/terminal";
@@ -40,8 +37,6 @@ const config: Phaser.Types.Core.GameConfig = {
         Level3Game,
         //Level4Game,
         Level5Game,
-        PlotTwist,
-        Secret,
         Win,
         GameOver,
     ],
